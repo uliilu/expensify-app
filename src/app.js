e24b15f0c5c0774344a5d1addc1123ca8cba3745
Uli von Homeyer <uli@Ulis-MacBook-Pro.local>
2017-11-20 15:14:46 +0100

    Expenses Dummydaten auskommentiert

--- src/app.js
+++ src/app.js
@@ -15,39 +15,40 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-store.subscribe(() => {
-    const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-    console.log(visibleExpenses);
-});
 
-console.log(store.getState());
+// store.subscribe(() => {
+//     const state = store.getState();
+//     const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+//     console.log(visibleExpenses);
+// });
 
-store.dispatch(addExpense({
-    description: 'Wasser und Meer',
-    amount: 50000,
-    createdAt: -56000
-}));
-store.dispatch(addExpense({
-    description: 'Haushaltsanschaffungen auf periodoscher Ebene',
-    amount: 43300,
-    createdAt: 56777
-}));
-store.dispatch(addExpense({
-    description: 'Eine Packung Ninjago Karten',
-    amount: 5000,
-    createdAt: -569777
-}));
-store.dispatch(addExpense({
-    description: 'Handy Hülle',
-    amount: 9900,
-    createdAt: 9956777
-}));
+// console.log(store.getState());
 
-// store.dispatch(setTextFilter('meer'));
-// // store.dispatch(setTextFilter('water'));
-//store.dispatch(sortBy('amount','desc'));
-store.dispatch(sortBy('date'));
+// store.dispatch(addExpense({
+//     description: 'Wasser und Meer',
+//     amount: 50000,
+//     createdAt: -56000
+// }));
+// store.dispatch(addExpense({
+//     description: 'Haushaltsanschaffungen auf periodoscher Ebene',
+//     amount: 43300,
+//     createdAt: 56777
+// }));
+// store.dispatch(addExpense({
+//     description: 'Eine Packung Ninjago Karten',
+//     amount: 5000,
+//     createdAt: -569777
+// }));
+// store.dispatch(addExpense({
+//     description: 'Handy Hülle',
+//     amount: 9900,
+//     createdAt: 9956777
+// }));
+
+// // store.dispatch(setTextFilter('meer'));
+// // // store.dispatch(setTextFilter('water'));
+// //store.dispatch(sortBy('amount','desc'));
+// store.dispatch(sortBy('date'));
 
 // setTimeout(() => {
 //     store.dispatch(setTextFilter(''));
